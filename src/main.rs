f670ac434d7132b457b1568c05a35505c38cf291
Jon1K0 <jonijuhi@gmail.com>
2025-06-29 15:39:36 +0200

    created window struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use sdl2::pixels::Color;
+
+use crate::window::SDL;
+pub mod window;
+
 fn main() {
+    let mut sdl = SDL::new(800, 800, "emulator".to_string());
+    let mut canvas = sdl.window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    'running: loop {
+        for event in sdl.event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }
+    }
+
     println!("Hello, world!");
 }
---
+++ src/window.rs
@@ -0,0 +1,22 @@
+use sdl2::Sdl;
+
+pub struct SDL {
+    context: Sdl,
+    pub window: sdl2::video::Window,
+    pub event_pump: sdl2::EventPump,
+}
+
+impl SDL {
+    pub fn new(width: u32, height: u32, name: String) -> Self {
+        let context: Sdl = sdl2::init().unwrap();
+        let vid = context.video().unwrap();
+        let win: sdl2::video::Window = vid.window(&name, width, height).build().unwrap();
+        let mut eventpump: sdl2::EventPump = context.event_pump().unwrap();
+
+        SDL {
+            context: context,
+            window: win,
+            event_pump: eventpump,
+        }
+    }
+}
